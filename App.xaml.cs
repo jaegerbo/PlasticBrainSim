f2cf74045b385795582f6549aab01c2f32b1e3c1
jaegerbo <Bodo_Jaeger@web.de>
2026-06-14 14:34:01 +0200

    Restore verified v0.1 baseline

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,4 @@
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -10,4 +10,3 @@ namespace PlasticBrainSim;
 public partial class App : Application
 {
 }
-
